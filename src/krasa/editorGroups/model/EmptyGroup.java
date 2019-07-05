79e5670fe3922e9dcf1c60cd445c10d10daeb158
Vojtěch Krása <vojta.krasa@gmail.com>
2019-07-06 01:26:27 +0200

    #3 added empty group

---
+++ src/krasa/editorGroups/model/EmptyGroup.java
@@ -0,0 +1,71 @@
+package krasa.editorGroups.model;
+
+import com.intellij.openapi.diagnostic.Logger;
+import com.intellij.openapi.project.Project;
+import org.jetbrains.annotations.NotNull;
+
+import javax.swing.*;
+import java.util.Collections;
+import java.util.List;
+
+public class EmptyGroup extends EditorGroup {
+	private static final Logger LOG = com.intellij.openapi.diagnostic.Logger.getInstance(EmptyGroup.class);
+	public static final String ID = "EmptyGroup";
+
+	@NotNull
+	@Override
+	public String getId() {
+		return ID;
+	}
+
+
+	@Override
+	public String switchTitle(Project project) {
+		return getTitle();
+	}
+
+	@Override
+	public String getTitle() {
+		return "Empty group";
+	}
+
+	@Override
+	public boolean isValid() {
+		return true;
+	}
+
+	@Override
+	public Icon icon() {
+		return null;
+	}
+
+	@Override
+	public void invalidate() {
+
+	}
+
+	@Override
+	public String getPresentableTitle(Project project, String presentableNameForUI, boolean showSize) {
+		return presentableNameForUI;
+	}
+
+	@Override
+	public int size(Project project) {
+		return 0;
+	}
+
+	@Override
+	public List<Link> getLinks(Project project) {
+		return Collections.emptyList();
+	}
+
+	@Override
+	public boolean isOwner(String ownerPath) {
+		return true;
+	}
+
+	@Override
+	public boolean equals(Object obj) {
+		return super.equals(obj) || obj instanceof EmptyGroup;
+	}
+}
