b11bd397e3f37da9b10753ffa01966dc374cc0f3
Vojtěch Krása <vojta.krasa@gmail.com>
2019-07-07 15:45:35 +0200

    removed newImpl

--- src/krasa/editorGroups/tabs2/newImpl/JBDefaultTabPainter.kt
+++
@@ -1,9 +0,0 @@
-// Copyright 2000-2019 JetBrains s.r.o. Use of this source code is governed by the Apache 2.0 license that can be found in the LICENSE file.
-package krasa.editorGroups.tabs2.newImpl
-
-import org.jetbrains.annotations.ApiStatus
-
-@Deprecated("Use krasa.editorGroups.tabs2.impl.JBDefaultTabPainter instead")
-@ApiStatus.ScheduledForRemoval(inVersion = "2019.3")
-open class JBDefaultTabPainter() : krasa.editorGroups.tabs2.impl.JBDefaultTabPainter() {
-}
