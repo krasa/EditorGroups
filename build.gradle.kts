417763e6e4c4789394e1c0cd59f1800904cf080a
Elior <heliosaian@gmail.com>
2023-07-22 12:01:15 +0300

    ...

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,72 +1,72 @@
 plugins {
-    id("java")
-    id("org.jetbrains.intellij") version "1.14.1"
-    id("org.jetbrains.kotlin.jvm") version "1.8.21"
+  id("java")
+  id("org.jetbrains.intellij") version "1.14.1"
+  id("org.jetbrains.kotlin.jvm") version "1.8.21"
 }
 
 group = "EditorGroups"
-version = "1.0"
+version = "1.1"
 
 tasks {
-    patchPluginXml {
-        sinceBuild.set("231")
-        untilBuild.set("241.*")
-        changeNotes.set(
-            buildString {
-                append("- Icons fix").append("<br>")
-            }
-        )
-    }
+  patchPluginXml {
+    sinceBuild.set("231")
+    untilBuild.set("241.*")
+    changeNotes.set(
+      buildString {
+        append("- Icons fix").append("<br>")
+      }
+    )
+  }
 
-    // Set the JVM compatibility versions
-    withType<JavaCompile> {
-        sourceCompatibility = "17"
-        targetCompatibility = "17"
-    }
+  // Set the JVM compatibility versions
+  withType<JavaCompile> {
+    sourceCompatibility = "17"
+    targetCompatibility = "17"
+  }
 
 
-    signPlugin {
-        certificateChain.set(System.getenv("CERTIFICATE_CHAIN"))
-        privateKey.set(System.getenv("PRIVATE_KEY"))
-        password.set(System.getenv("PRIVATE_KEY_PASSWORD"))
-    }
+  signPlugin {
+    certificateChain.set(System.getenv("CERTIFICATE_CHAIN"))
+    privateKey.set(System.getenv("PRIVATE_KEY"))
+    password.set(System.getenv("PRIVATE_KEY_PASSWORD"))
+  }
 
-    publishPlugin {
-        token.set(System.getenv("PUBLISH_TOKEN"))
-    }
+  publishPlugin {
+    token.set(System.getenv("PUBLISH_TOKEN"))
+  }
 
-    buildSearchableOptions {
-        enabled = false
-    }
+  buildSearchableOptions {
+    enabled = false
+  }
 
-    compileKotlin {
-        kotlinOptions.jvmTarget = "17"
-    }
+  compileKotlin {
+    kotlinOptions.jvmTarget = "17"
+  }
 
-    compileTestKotlin {
-        kotlinOptions.jvmTarget = "17"
-    }
+  compileTestKotlin {
+    kotlinOptions.jvmTarget = "17"
+  }
 }
 
 repositories {
-    mavenCentral()
+  mavenCentral()
 }
 
 // Configure Gradle IntelliJ Plugin - read more: https://github.com/JetBrains/gradle-intellij-plugin
 intellij {
-    version.set("LATEST-EAP-SNAPSHOT")
-    type.set("IU") // Target IDE Platform
+  version.set("LATEST-EAP-SNAPSHOT")
+  type.set("IU") // Target IDE Platform
 
-    plugins.set(listOf("java"))
+  plugins.set(listOf("java"))
 }
 
 
 dependencies {
 // https://mvnrepository.com/artifact/commons-io/commons-io
-    implementation("commons-io:commons-io:2.11.0")
+  implementation("commons-io:commons-io:2.11.0")
 
-    // https://mvnrepository.com/artifact/org.apache.commons/commons-lang3
-    implementation("org.apache.commons:commons-lang3:3.12.0")
+  // https://mvnrepository.com/artifact/org.apache.commons/commons-lang3
+  implementation("org.apache.commons:commons-lang3:3.12.0")
 
 }
 
