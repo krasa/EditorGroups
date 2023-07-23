b00f1fffa0101aca67d2712f996055c2ccac064b
Elior <heliosaian@gmail.com>
2023-07-23 21:31:10 +0300

    add docs

---
+++ src/main/java/krasa/editorGroups/Example.java
@@ -0,0 +1,11 @@
+package krasa.editorGroups;
+
+public class Example {
+  public static void main(String[] args) {
+    System.out.println("Hello World!");
+  }
+
+  public static void main(String[] args) {
+    System.out.println("Hello World!");
+  }
+}
